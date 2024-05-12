3ddd0fadf133c1387393f62d8aebbae79d661bf0
Anirudh Menon <anirudhmenon@Anirudhs-MacBook-Pro.local>
2024-05-12 17:42:45 +0200

    Add aditional features

--- lib/main.dart
+++ lib/main.dart
@@ -27,12 +27,15 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   TextEditingController textController = TextEditingController(); 
   String reversedSequence = ''; 
+  String reverseSequence = '';
+  String complementSequence = '';
+  String sequence = '';
 
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       navigationBar: const CupertinoNavigationBar(
-        middle: Text("Enter the DNA sequence you want to reverse."),
+        middle: Text("Enter the DNA sequence you want to perform the operations on."),
       ),
       child: Center(
         child: Padding(
@@ -57,25 +60,41 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               const SizedBox(height: 20.0),
               CupertinoButton.filled(
-                child: const Text('Reverse'),
+                child: const Text('Go!'),
                 onPressed: () {
                   String input = textController.text.toUpperCase(); 
                   setState(() {
-                    reversedSequence = _reversecomplementDNA(input); 
+                    reversedSequence = _reversecomplementDNA(input);
+                    reverseSequence = _reverseDNA(input);
+                    complementSequence = _complementDNA(input);
+                    sequence = input;
                     textController.clear(); 
                   });
                 },
               ),
               const SizedBox(height: 20.0),
+              const Text("Sequence", style: TextStyle(fontSize: 18, color: Color.fromARGB(255, 3, 87, 156)),),
+              const SizedBox(height: 20.0),
+              Text(sequence, style: const TextStyle(fontSize: 28.0, color: Colors.red, fontWeight: FontWeight.bold),),
+              const SizedBox(height: 20.0),
               const Text(
                 'Reverse-Complement:',
                 style: TextStyle(fontSize: 18.0, color: Colors.blue),
               ),
-              const SizedBox(height: 10.0),
+              const SizedBox(height: 20.0),
               Text(
                 reversedSequence,
                 style: const TextStyle(fontSize: 28.0, color: Colors.red, fontWeight: FontWeight.bold),
               ),
+              const SizedBox(height: 20),
+              const Text("Reverse:", style: TextStyle(fontSize: 18, color: Colors.cyanAccent),),
+              const SizedBox(height: 20),
+              Text(reverseSequence, style : const TextStyle(color: Colors.red, fontSize: 28.0, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 20,),
+              const Text("Complement:", style: TextStyle(fontSize: 18, color: Color.fromARGB(255, 0, 255, 128)),),
+              const SizedBox(height: 20,),
+              Text(complementSequence, style : const TextStyle(color: Colors.red, fontSize: 28.0, fontWeight: FontWeight.bold)),
+              const SizedBox(height: 20,),
             ],
           ),
         ),
@@ -83,17 +102,45 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-String _reversecomplementDNA(String sequence) {
-  Map<String, String> complementMap = {
-    'A': 'T',
-    'T': 'A',
-    'C': 'G',
-    'G': 'C',
-  };
+  String _reversecomplementDNA(String sequence) {
+    if (sequence.contains(RegExp(r'[^ATGC]'))) {
+      return "Invalid nucleotide(s)";
+    }
+    Map<String, String> complementMap = {
+      'A': 'T',
+      'T': 'A',
+      'C': 'G',
+      'G': 'C',
+    };
 
-  return sequence.split('').toList().reversed.map((nucleotide) {
-    return complementMap.containsKey(nucleotide) ? complementMap[nucleotide] : "Invalid_nucleotide ";
-  }).join();
-}
+    return sequence.split('').toList().reversed.map((nucleotide) {
+    return complementMap[nucleotide];
+    }).join();
+  }
+
+  String _reverseDNA(String sequence) {
+    if (sequence.contains(RegExp(r'[^ATGC]'))) {
+      return "Invalid nucleotide(s)";
+    }
+
+    return sequence.split('').reversed.join();
+  }
+
+  String _complementDNA(String sequence) {
+    if (sequence.contains(RegExp(r'[^ATGC]'))) {
+      return "Invalid nucleotide(s)";
+    }
+    Map<String, String> complementMap = {
+      'A': 'T',
+      'T': 'A',
+      'C': 'G',
+      'G': 'C',
+    };
+
+    return sequence.split('').toList().map((nucleotide) {
+    return complementMap[nucleotide];
+    }).join();    
+
+  }
 
 }
