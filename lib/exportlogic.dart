58e3b9c154fa774e02e7488eb4893d941baac669
Anirudh Menon <thenasaplusit@proton.me>
2025-04-19 16:25:44 +0200

    cleanup the code a bit

--- lib/exportlogic.dart
+++ lib/exportlogic.dart
@@ -3,54 +3,57 @@ import 'dart:io';
 import 'shared.dart';
 import 'package:csv/csv.dart';
 
-Future<void> exportLogic(BuildContext context, String sequence,
-      String reverseSequence, String reversedSequence,
-      String complementSequence) async {
-    if (sequence.isEmpty) {
-      Shared.showErrorDialog(context, 'No valid DNA sequence detected.');
-    } else {
-      List<List<String>> rows = [
-        ['Input', 'Reverse', 'Complement', 'Reverse-Complement'],
-        [sequence, reverseSequence, complementSequence, reversedSequence],
-      ];
-      String csv = const ListToCsvConverter().convert(rows);
+Future<void> exportLogic(
+    BuildContext context,
+    String sequence,
+    String reverseSequence,
+    String reversedSequence,
+    String complementSequence) async {
+  if (sequence.isEmpty) {
+    Shared.showErrorDialog(context, 'No valid DNA sequence detected.');
+  } else {
+    List<List<String>> rows = [
+      ['Input', 'Reverse', 'Complement', 'Reverse-Complement'],
+      [sequence, reverseSequence, complementSequence, reversedSequence],
+    ];
+    String csv = const ListToCsvConverter().convert(rows);
 
-      String? path = await Shared.makeFileName('dna_sequence');
+    String? path = await Shared.makeFileName('dna_sequence');
 
-      if (path != null) {
-        try {
-          File file = File(path);
-          await file.writeAsBytes(csv as List<int>);
-        } catch (error) {
-          Shared.showErrorDialog(context, 'An error occurred while exporting : $error');
-        }
-
-        if (context.mounted) {
-          showDialog<String>(
-            context: context,
-            builder: (BuildContext context) =>
-                AlertDialog(
-                  title: const Text('Export Successful'),
-                  content: Text('CSV exported to: $path'),
-                  actions: <Widget>[
-                    TextButton(
-                      onPressed: () {
-                        if (context.mounted) {
-                          Navigator.pop(context, 'OK');
-                        } else {
-                          exit(0);
-                        }
-                      },
-                      child: const Text('OK'),
-                    ),
-                  ],
-                ),
-          );
-        } else {
-          exit(0);
-        }
-      } else {
-        Shared.showErrorDialog(context, 'Failed to generate file path.');
+    if (path != null) {
+      try {
+        File file = File(path);
+        await file.writeAsString(csv);
+      } catch (error) {
+        Shared.showErrorDialog(
+            context, 'An error occurred while exporting : $error');
       }
+
+      if (context.mounted) {
+        showDialog<String>(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+            title: const Text('Export Successful'),
+            content: Text('CSV exported to: $path'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  if (context.mounted) {
+                    Navigator.pop(context, 'OK');
+                  } else {
+                    exit(0);
+                  }
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          ),
+        );
+      } else {
+        exit(0);
+      }
+    } else {
+      Shared.showErrorDialog(context, 'Failed to generate file path.');
     }
   }
+}
--- lib/shared.dart
+++ lib/shared.dart
@@ -1,30 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:file_selector/file_selector.dart';
 import 'package:intl/intl.dart';
+import 'package:path_provider/path_provider.dart';
+
 class Shared {
   // Helper function to show error dialogs
   static void showErrorDialog(BuildContext context, String message) {
     showDialog<String>(
       context: context,
-      builder: (BuildContext context) =>
-          AlertDialog(
-            title: const Text('Error'),
-            content: Text(message),
-            actions: <Widget>[
-              TextButton(
-                onPressed: () => Navigator.pop(context, 'OK'),
-                child: const Text('OK'),
-              ),
-            ],
+      builder: (BuildContext context) => AlertDialog(
+        title: const Text('Error'),
+        content: Text(message),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.pop(context, 'OK'),
+            child: const Text('OK'),
           ),
+        ],
+      ),
     );
   }
 
-    static Future<String?> makeFileName(String name) async {
-    final String? directory = await getDirectoryPath();
+  static Future<String?> makeFileName(String name) async {
+    String? directory = await getDirectoryPath();
     DateTime now = DateTime.now();
     String formattedDate = DateFormat('yyyy-MM-dd_HH-mm-ss').format(now);
-      final path = '$directory/$formattedDate-$name.csv';
-      return path;
+    dynamic docdir = await getApplicationDocumentsDirectory();
+    directory ??= docdir.path;
+    final path = '$directory/$formattedDate-$name.csv';
+    return path;
   }
 }
